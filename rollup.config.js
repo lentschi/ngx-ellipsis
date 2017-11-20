3e2c8bef6189cbb28d084b5bcf208f316b7d1139
Florian Lentsch <office@florian-lentsch.at>
2017-11-20 19:37:22 +0100

    Fixes #1

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 export default {
   entry: 'dist/index.js',
-  dest: 'dist/bundles/ngxEllipsis.umd.js',
+  dest: 'dist/ngxEllipsis.umd.js',
   sourceMap: false,
   format: 'umd',
   moduleName: 'ng.ngxEllipsis',
