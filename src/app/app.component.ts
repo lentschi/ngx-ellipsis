0a2218a6a20d1b4c118235a4ae8fc49712dd99aa
João Portela <joaoportela@users.noreply.github.com>
2018-10-23 15:27:23 +0100

    Add more samples to ngx-ellipsis-demo.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngx-ellipsis-demo';
+  longText = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna \
+              aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea\
+              takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy \
+              eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo \
+              dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.';
+  // the next variables would cause an error in the htmlEscape method before the new checks.
+  number = 12.4564564564564564;
+  undefinedText: string = undefined;
+  nullText: string = null;
 }
