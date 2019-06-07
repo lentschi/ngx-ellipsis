30e64915937aa231b9dcd1fe0aa2977422dab06b
Florian Lentsch <office@florian-lentsch.at>
2019-06-07 12:04:04 +0200

    Added some more samples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,8 +12,5 @@ export class AppComponent {
               takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy \
               eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo \
               dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.';
-  // the next variables would cause an error in the htmlEscape method before the new checks.
   number = 12.4564564564564564;
-  undefinedText: string = undefined;
-  nullText: string = null;
 }
